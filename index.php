e7727c1533473184490e17b03a738017ebec5162
Martin Knotek <martinkntk@gmail.com>
2015-10-10 10:59:00 +0200

    login screen

---
+++ index.php
@@ -0,0 +1,15 @@
+<? echo "Informacni system Spravce Koncertu"; ?>
+<br><br>
+<? echo "Prosim, prohlaste se:"; ?>
+<br><br>
+
+<form method="post" action="power.php">
+	Login:<br>
+	<input type="text" name="login">
+	<br>
+	Heslo:<br>
+	<input type="password" name="passwd">
+	<br>
+	<input type="submit" value="Prihlasit" >	
+</form>
+
