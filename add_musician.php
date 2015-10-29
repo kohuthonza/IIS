7bd9780ec201633a1a283589b600488c12c2333e
Martin Knotek <martinkntk@gmail.com>
2015-10-29 12:36:38 +0100

    showing, deleting, updating, adding musicians in now working

---
+++ add_musician.php
@@ -0,0 +1,78 @@
+<?php
+include_once('functions.php');
+session_save_path("./tmp");
+session_start();
+if($_SESSION['role'] != 2){
+	$val = $_SESSION['role'];
+	die("Na tuto stranku nemate pristup! Vase role: $val. Potrebna role: 2<br>
+		<a href=\"index.php\">Zpet na hlavni stranu</a>
+			");
+}
+?>
+
+
+
+<html>
+<title>
+Pridat hudebnika
+</title>
+
+<body>
+
+Prosim, vyplnte udaje o hudebnikovi:
+<form method="post" action="add_musician.php">
+	Jmeno:<br>
+	<input type="text" name="name">*<br><br>
+	Prijmeni:<br>
+	<input type="text" name="sname">*<br><br>
+	Rodne cislo:<br>
+	<input type="text" name="rc">*<br><br>
+	Telefon:<br>
+	<input type="text" name="phone">*<br><br>
+	Email:<br>
+	<input type="text" name="email">*<br><br>
+	Mesto (adresa):<br>
+	<input type="text" name="town">*<br><br>
+	<input type="submit" value="Pridat hudebnika">
+</form>
+
+<br> Udaje oznacene * jsou povinne <br><br>
+<a href="index.php">Zpet na hlavni stranu</a><br>
+<a href="personel.php">Zpet na hudebniky</a>
+<br>
+
+</body>
+</html>
+
+
+<?php
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+	if(!db_connect())
+		die('Nepodarilo se pripojit k databazi');
+
+	$name = htmlspecialchars($_POST['name']);
+	$sname = htmlspecialchars($_POST['sname']);
+	$rc = htmlspecialchars($_POST['rc']);
+
+	$phone = htmlspecialchars($_POST['phone']);
+	$email = htmlspecialchars($_POST['email']);
+	$town = htmlspecialchars($_POST['town']);
+	
+			
+	$db = $_SESSION['db'];
+
+	$cmmnd = "insert into musicians (Name, SName, RC, Phone, Email, Town) values ('$name', '$sname', '$rc', '$phone', '$email', '$town')";
+			
+	if(mysql_query($cmmnd, $db)){
+		echo "pridano";
+	}
+	else{
+		echo "error: ", mysql_error($db);
+		die();
+	}
+
+	mysql_close($db);
+}
--- personel.php
+++ personel.php
@@ -1,3 +1,18 @@
+<?php
+session_save_path("./tmp");
+session_start();
+include_once('functions.php');
+
+
+if($_SESSION['role'] != 2){
+	$val = $_SESSION['role'];
+	die("Na tuto stranku nemate pristup! Vase role: $val. Potrebna role: 2<br>
+		<a href=\"index.php\">Zpet na hlavni stranu</a>
+			");
+}
+?>
+
+
 <html>
 <head>
 
@@ -9,41 +24,35 @@ Zaznamy o lidech:<br>
 
 <a href="index.php">Zpet na hlavni stranu</a>
 <br>
-<a href="index.php">Pridat hudebnika</a>
+<a href="add_musician.php">Pridat hudebnika</a>
 <table border=1>
 		<tr>
 		<th>Jmeno</th>
+		<th>Prijmeni</th>
 		<th>Rodne cislo</th>
-		<th>Adresa</th>
+		<th>Mesto</th>
 		<th>Telefon</th>
 		<th>Email</th>
+		<th><form method=post action=personel.php><input type=submit name=clear value=Vycistit></form></td>
 		</tr>
 <form method=post action=personel.php>
 	<tr>
-	<td><input type=text name=Fname value=> </td>
-	<td><input type=text name=Frc value=> </td>
-	<td><input type=text name=Faddr value=> </td>
-	<td><input type=text name=Fphone value=> </td>
-	<td><input type=text name=Femail value=> </td>
+	<td><input type=text name=Fname value='<?php if(isset($_POST['filter']) and !isset($_POST['clear']) and !empty($_POST['Fname'])){ echo $_POST['Fname'];}?>'> </td>
+	<td><input type=text name=Fsname value='<?php if(isset($_POST['filter']) and !isset($_POST['clear']) and !empty($_POST['Fsname'])){ echo $_POST['Fsname'];}?>'> </td>
+	<td><input type=text name=Frc value=<?php if(isset($_POST['filter']) and !isset($_POST['clear']) and !empty($_POST['Frc'])){ echo $_POST['Frc'];}?>> </td>
+	<td><input type=text name=Ftown value='<?php if(isset($_POST['filter']) and !isset($_POST['clear']) and !empty($_POST['Ftown'])){ echo $_POST['Ftown'];}?>'> </td>
+	<td><input type=text name=Fphone value='<?php if(isset($_POST['filter']) and !isset($_POST['clear']) and !empty($_POST['Fphone'])){ echo $_POST['Fphone'];}?>'> </td>
+	<td><input type=text name=Femail value=<?php if(isset($_POST['filter']) and !isset($_POST['clear']) and !empty($_POST['Femail'])){ echo $_POST['Femail'];}?>> </td>
 	<td><input type=submit name=filter value=Filtrovat></td>
 </form>
 </table>
 
 	<br><br>
 <?php
-session_save_path("./tmp");
-session_start();
-include_once('functions.php');
 
 if(!isset($_SESSION['logged'])){
 	Header('Location: index.php');
 
-}
-if($_SESSION['role'] != 2){
-	$val = $_SESSION['role'];
-	die("Na tuto stranku nemate pristup! Vase role: $val. Potrebna role: 2<br>
-		<a href=\"index.php\">Zpet na hlavni stranu</a>
-			");
 }
 
 if(!db_connect())
@@ -51,9 +60,14 @@ if(!db_connect())
 
 //if the page recieved a command to update a specific row of the table
 if(isset($_POST['updatebtn'])){
-	$updateQry = "update musicians set Name='$_POST[name]', Address='$_POST[addr]', RC='$_POST[rc]', Phone='$_POST[phone]', Email='$_POST[email]' where RC='$_POST[hidden]'";
+	$updateQry = "update musicians set Name='$_POST[name]', SName='$_POST[Sname]', Town='$_POST[town]', RC='$_POST[rc]', Phone='$_POST[phone]', Email='$_POST[email]' where RC='$_POST[hidden]'";
 
 	mysql_query($updateQry, $_SESSION['db']);
+};
+
+if(isset($_POST['deletebtn'])){
+	$deleteQry = "delete from musicians where RC='$_POST[hidden]'";
+	mysql_query($deleteQry, $_SESSION['db']);
 };
 
 $SQL = "select * from musicians";
@@ -64,12 +78,16 @@ if(isset($_POST['filter'])){
 		$SQL = $SQL . " $where Name='$_POST[Fname]'";
 		$where = " and";
 	}
+	if(!empty($_POST['Fsname'])){
+		$SQL = $SQL . " $where SName='$_POST[Fsname]'";
+		$where = " and";
+	}
 	if(!empty($_POST['Frc'])){
 		$SQL = $SQL . " $where RC='$_POST[Frc]'";
 		$where = " and";			
 	}
-	if(!empty($_POST['Faddr'])){
-		$SQL = $SQL . " $where Address='$_POST[Faddr]'";
+	if(!empty($_POST['Ftown'])){
+		$SQL = $SQL . " $where Town='$_POST[Ftown]'";
 		$where = " and";
 	}
 	if(!empty($_POST['Fphone'])){
@@ -80,16 +98,18 @@ if(isset($_POST['filter'])){
 		$SQL = $SQL . " $where Email='$_POST[Femail]'";
 		$where = " and";
 	}
+	
 };
-
+#echo $SQL;
 
 $retval = mysql_query($SQL, $_SESSION['db']);
 
 echo "<table border=1>
 		<tr>
 		<th>Jmeno</th>
+		<th>Prijmeni</th>
 		<th>Rodne cislo</th>
-		<th>Adresa</th>
+		<th>Mesto</th>
 		<th>Telefon</th>
 		<th>Email</th>
 		</tr>";
@@ -97,13 +117,15 @@ echo "<table border=1>
 while($row = mysql_fetch_array($retval)){
 	echo "<form method=post action=personel.php>";
 	echo "<tr>";
-	echo "<td>" . "<input type=text name=name value=" . $row['Name'] . " </td>";
+	echo "<td>" . "<input type=text name=name value='" . $row['Name'] . "' </td>";
+	echo "<td>" . "<input type=text name=Sname value='" . $row['SName'] . "' </td>";
 	echo "<td>" . "<input type=text name=rc value=" . $row['RC'] . " </td>";
-	echo "<td>" . "<input type=text name=addr value=" . $row['Address'] . " </td>";
-	echo "<td>" . "<input type=text name=phone value=" . $row['Phone'] . " </td>";
+	echo "<td>" . "<input type=text name=town value='" . $row['Town'] . "' </td>";
+	echo "<td>" . "<input type=text name=phone value='" . $row['Phone'] . "' </td>";
 	echo "<td>" . "<input type=text name=email value=" . $row['Email'] . " </td>";
 	echo "<td>" . "<input type=hidden name=hidden value=" . $row['RC'] . " </td>";
 	echo "<td>" . "<input type=submit name=updatebtn value=Upravit" . " </td>";
+	echo "<td>" . "<input type=submit name=deletebtn value=Odstranit" . " </td>";
 	echo "</tr>";
 	echo "</form>";
 }
