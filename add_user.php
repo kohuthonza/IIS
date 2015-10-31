5ee0c388cc6a6d644e609380b1e7414cc25da2b0
Honza <kohut.honza@seznam.cz>
2015-10-31 10:41:44 +0100

    change

--- add_user.php
+++ add_user.php
@@ -1,44 +1,119 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+include_once('functions.php');
+session_save_path("./tmp");
+session_start();
+?>
+
 <html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="add_user.css" rel="stylesheet">
 <title>
-Pridat uzivatele
+Přidat uživatele
 </title>
+</head>
 
-<body>
+<body >
 
-Prosim, vyplnte udaje o uzivateli:
 <form method="post" action="add_user.php">
-	Jmeno:<br>
-	<input type="text" name="name">*<br><br>
-	Login:<br>
-	<input type="text" name="login">*<br><br>
-	Heslo:<br>
-	<input type="password" name="passwd">*<br><br>
-	Datum Narozeni:<br><font size="2">(Den/Mesic/Rok)</font><br>
-	<input type="text" name="day" size="5">/
-	<input type="text" name="month" size="5">/
-	<input type="text" name="year" size="10"><br><br>
-	Role:<br>
-	<select required name="role">
-		<option value="1">Spravce personalu</option>
-		<option value="2">Spravce koncertu</option>
-		<option value="3">Spravce skladeb</option>
-	</select>*
-	<br><br>
-	<input type="submit" value="Pridat uzivatele">
-</form>
-
-<br> Udaje oznacene * jsou povinne <br><br>
-<a href="index.php">Zpet na hlavni stranu</a>
-<br>
-
+	
+	<div class="container-fluid">
+	<form class="form-horizontal" data-toggle="validator">
+		<div class="form-group row">
+			&nbsp;
+		</div>
+		<div class="form-group row">
+			<label for="first_name" class="col-lg-1 col-lg-offset-2 control-label input-lg text-right">Jméno</label>
+			<div class="col-lg-4">
+				<input type="text" class="form-control input-lg" id="first_name" placeholder="Jméno" name="name" required>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="second_name" class="col-lg-1 col-lg-offset-2 control-label input-lg text-right">Příjmení</label>
+			<div class="col-lg-4">
+				<input type="text" class="form-control input-lg" id="second_name" placeholder="Příjmení" required>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="birth_date" class="col-lg-2 col-lg-offset-1 control-label input-lg text-right">Datum narození</label>
+			<div class="col-lg-1">
+				<select required class="form-control" id="birth_date" name="day">
+					<option value="" disabled selected>DD</option>
+					<?php for ($i = 1; $i <= 31; $i++) : ?>
+						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+					<?php endfor; ?>
+				</select>
+			</div>
+			<div class="col-lg-1">
+				<select required class="form-control" name="month">
+					 <option value="" disabled selected>MM</option>
+					<?php for ($i = 1; $i <= 12; $i++) : ?>
+						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+					<?php endfor; ?>
+				</select>
+			</div>
+			<div class="col-lg-2">
+				<select required class="form-control" name="year">
+				<option value="" disabled selected>RRRR</option>
+					<?php for ($i = 1880; $i <= 2015; $i++) : ?>
+						<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+					<?php endfor; ?>
+				</select>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="tel" class="col-lg-1 col-lg-offset-2 control-label input-lg text-right">Telefon</label>
+			<div class="col-lg-4">
+				<input type="tel" class="form-control input-lg" id="tel" placeholder="Telefon" required>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="role" class="col-lg-1 col-lg-offset-2 control-label input-lg text-right">Role</label>
+			<div class="col-lg-4">
+				<select required class="form-control input-lg" id="role" name="role">
+					<option value="" disabled selected>Vyberte roli</option>
+					<option value="1">Správce personálu</option>
+					<option value="2">Správce koncertů</option>
+					<option value="3">Správce skladeb</option>	
+				</select>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="login" class="col-lg-2 col-lg-offset-1 control-label input-lg text-right">Přihlašovací jméno</label>
+			<div class="col-lg-4">
+				<input type="text" class="form-control input-lg" id="login" placeholder="Přihlašovací jméno" name="login" required>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="passwd" class="col-lg-1 col-lg-offset-2 control-label input-lg text-right">Heslo</label>
+			<div class="col-lg-4">
+				<input type="password" class="form-control input-lg" id="passwd" placeholder="Heslo" name="passwd" required>
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-lg-2 col-lg-offset-3">
+				<button class="btn btn-success btn-lg" type="submit">Přidat uživatele</button>
+			</div>
+			<div class="col-lg-2">
+				<a class="btn btn-warning btn-lg" href="index.php" role="button">Zpět na hlavní stranu</a>
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-lg-4 col-lg-offset-3">
+				<strong>*Všechny údaje jsou povinné</strong>
+			</div>
+		</div>
+	</form>
+	</div>
 </body>
 </html>
 
 
 <?php
-include_once('functions.php');
-session_save_path("./tmp");
-session_start();
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 	if(!db_connect())
