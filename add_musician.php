bc0947a79090e2520306d7d59206b16281e802a4
Martin Knotek <martinkntk@gmail.com>
2015-11-11 10:24:59 +0100

    roles fixed

--- add_musician.php
+++ add_musician.php
@@ -2,7 +2,7 @@
 include_once('functions.php');
 session_save_path("./tmp");
 session_start();
-if($_SESSION['role'] != 2){
+if($_SESSION['role'] != 1){
 	$val = $_SESSION['role'];
 	die("Na tuto stranku nemate pristup! Vase role: $val. Potrebna role: 2<br>
 		<a href=\"index.php\">Zpet na hlavni stranu</a>
