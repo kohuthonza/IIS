40a119989d3c0b56a1d7bd266a2cadac8514390e
Honza <kohut.honza@seznam.cz>
2015-11-13 00:01:02 +0100

    design table

--- personel.php
+++ personel.php
@@ -23,7 +23,6 @@ if($_SESSION['role'] != 1){
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="personel.css" rel="stylesheet">
 
 <title>Seznam hudebníků</title>
 </head>
